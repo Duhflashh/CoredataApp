5dd5b1e88c35387cc0961430129c6ce13ff87832
Duhflashh <73899377+Duhflashh@users.noreply.github.com>
2021-10-25 15:40:47 -0400

    setup project files and imported json

--- CoredataApp/AppDelegate.swift
+++ CoredataApp/AppDelegate.swift
@@ -15,7 +15,69 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Preload data if needed
+        preloadData()
         return true
+    }
+    
+    // MARK: - Preload Data
+    
+    private func preloadData() {
+        
+        // Reference to user defaults
+        let defaults = UserDefaults.standard
+        
+        // Reference to Core Data Context
+        let context = persistentContainer.viewContext
+        
+        // Check if this is the first launch
+        if defaults.bool(forKey: Constants.PRELOAD_DATA) == false {
+            
+            // If so, then parse the json file into core data
+            
+            // Get a path to the local json file
+            let path = Bundle.main.path(forResource: "PreloadedData", ofType: "json")
+            
+            // Check that path isnt nil
+            guard path != nil else {
+                print("Could'nt get path to local json file")
+                return
+            }
+            
+            // Create a URL to it
+            let url = URL(fileURLWithPath: path!)
+            
+            do {
+                // Get the data for the file
+                let data = try Data(contentsOf: url)
+                
+                //Try turning the data into a json object
+                let jsonArray = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! [[String:Any]]
+                
+                // Loop through the json objects
+                for d in jsonArray {
+                    
+                    // Create a place object and populate properties
+                    let p = Place(context: context)
+                    p.name = d["name"] as? String
+                    p.imageName = d["imagename"] as? String
+                    p.address = d["address"] as? String
+                    p.summary = d["summary"] as? String
+                    p.lat = d["lat"] as! Double
+                    p.long = d["long"] as! Double
+                }
+                
+            }
+            catch{}
+            
+            // Save the data
+            self.saveContext()
+            
+            // Set the preload data flag to true
+            defaults.set(true, forKey: Constants.PRELOAD_DATA)
+        }
+  
     }
 
     // MARK: UISceneSession Lifecycle
---
+++ CoredataApp/Models/Place+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Place+CoreDataClass.swift
+//  CoredataApp
+//
+//  Created by Michael Duberry on 10/24/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Place)
+public class Place: NSManagedObject {
+
+}
